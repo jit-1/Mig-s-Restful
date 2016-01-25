411042afb4bd3716ce987ab9376f4c7d5f8fc55e
Jit <prithvirajpramanik@yahoo.co.in>
2016-01-25 22:23:05 +0530

    Change Error 401 to 403 to avoid Login Dialog Box

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def get_password(username):
 
 @auth.error_handler
 def unauthorized():
-	return make_response(jsonify({'error': 'Unauthorized Access'}), 401)
+	return make_response(jsonify({'error': 'Unauthorized Access'}), 403
 
 
 
